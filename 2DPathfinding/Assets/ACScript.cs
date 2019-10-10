868a97e0e43fe69d16c5f9aa8ec6c47741b6aed8
MrAdelmund <mathew.adelmund@ksd.org>
2019-10-09 21:03:18 -0700

    Adding pathfinding for enemies, player animator controller, tilemap level

---
+++ 2DPathfinding/Assets/ACScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ACScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        anim.SetFloat("y", rb.velocity.y);
+        anim.SetBool("Run", rb.velocity.x != 0);
+        anim.SetBool("Grounded", GetComponent<PlayerMovement>().IsGrounded());
+        
+        if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            renderer.flipX = true;
+        }else if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            renderer.flipX = false;
+        }
+        
+    }
+}
---
+++ 2DPathfinding/Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 4.0f;
+    public float jumpForce = 2.0f;
+    Rigidbody2D rb;
+    bool grounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce * 50));
+
+        }
+    }
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Vector2 vel = rb.velocity;
+        vel.x = x * moveSpeed;
+        rb.velocity = vel;
+        
+    }
+    public bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.55f * transform.lossyScale.y);
+        Debug.DrawRay(transform.position, new Vector2(0, -0.55f * transform.lossyScale.y), Color.green);
+        grounded = hit.collider != null && hit.collider.gameObject.name != gameObject.name;
+
+
+        return grounded;
+    }
+}
